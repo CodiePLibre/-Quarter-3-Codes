5c9c207d2c9dc3cbc161eb3d98ca011a67fd30d4
CodiePLibre <codiejohn.libre@smc.pshs.edu.ph>
2026-02-01 13:50:04 +0800

    Add student record input and display functionality

---
+++ LibreCodie_CS2_ActSet1.py
@@ -0,0 +1,10 @@
+student = {}
+
+student["name"] = input("Enter your name: ")
+student["age"] = int(input("Enter your age: "))
+student["subject"] = input("Emter you favorite subject: ")
+
+print("\nStudent Record:")
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Favorite Subject:", student["subject"])
